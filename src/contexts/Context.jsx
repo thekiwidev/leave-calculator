3858bedac8a707dc2d16f5566ad99f85178a2614
thekiwidev <adedotungabriel20@gmail.com>
2023-03-25 21:48:18 +0100

    fixed the leave entitlement input bug

--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -1,9 +1,22 @@
-import { addBusinessDays, format, parseISO, sub } from "date-fns";
+import { addBusinessDays, format, isExists, parseISO, sub } from "date-fns";
 import { createContext, useEffect, useState } from "react";
 
 export const Context = createContext();
 
 const ContextProvider = ({ children }) => {
+  // ! LEAVE COMMENCMENT DATE
+  const [publicHolidays, setPublicHolidays] = useState([
+    // format(parseISO("2023-04-07"), `yyy M d`),
+    // format(parseISO("2023-04-10"), `yyy M d`),
+    // format(parseISO("2023-04-21"), `yyy M d`),
+    {
+      date: `2023-04-07`,
+      day: format(parseISO(`2023-04-07`), `d`),
+      month: format(parseISO(`2023-04-07`), `M`),
+      year: format(parseISO(`2023-04-07`), `yyyy`),
+    },
+  ]);
+
   // ! LEAVE COMMENCMENT DATE
   const [startDate, setStartDate] = useState(format(new Date(), `yyyy-MM-dd`));
 
@@ -29,7 +42,8 @@ const ContextProvider = ({ children }) => {
   // * Update the leave entitlement with inputs
   // ! LEAVE ENTITLEMENTS
   const updateLeaveEntitlement = (value) => {
-    setLeaveEntitlement(parseInt(value));
+    setLeaveEntitlement(value);
+    console.log(value);
   };
 
   // * Update the leave entitlement with the date picker
@@ -41,20 +55,30 @@ const ContextProvider = ({ children }) => {
   // * calculate the leave based on the start date and the leave entitlement
   // ! CALCULATE LEAVE
   const calculate = (date, amount) => {
-    let commencementDate = sub(parseISO(date), { days: 1 });
+    if (amount >= 1) {
+      let commencementDate = sub(parseISO(date), { days: 1 });
 
-    let expDate = addBusinessDays(commencementDate, amount);
+      let expDate = addBusinessDays(commencementDate, amount);
 
-    setEndDate(format(addBusinessDays(expDate, 1), `yyyy-MM-dd`));
+      setEndDate(format(addBusinessDays(expDate, 1), `yyyy-MM-dd`));
 
-    setExpirationDate(format(expDate, `do MMMM yyyy`));
+      setExpirationDate(format(expDate, `do MMMM yyyy`));
 
-    setResumptionDate({
-      date: `${format(parseISO(endDate), `do`)}`,
-      day: `${format(parseISO(endDate), `eeee`)}`,
-      month: `${format(parseISO(endDate), `MMMM`)}`,
-      year: `${format(parseISO(endDate), `yyyy`)}`,
-    });
+      setResumptionDate({
+        date: `${format(parseISO(endDate), `do`)}`,
+        day: `${format(parseISO(endDate), `eeee`)}`,
+        month: `${format(parseISO(endDate), `MMMM`)}`,
+        year: `${format(parseISO(endDate), `yyyy`)}`,
+      });
+
+      //! the part to skip Public Holidays
+      publicHolidays.map((ph) => {
+        console.log(
+          isExists(parseInt(ph.year), parseInt(ph.month), parseInt(ph.day))
+        );
+        console.log(ph);
+      });
+    }
   };
 
   // * calculate the leave on load and changed value of the start date and the leave entitlement
